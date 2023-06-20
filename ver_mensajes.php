ab1f75003b5e4bc2d5befbdf316a3d9081f6fc3b
Basxn18 <elbasanuwu@gmail.com>
2023-06-20 01:03:35 -0500

    as
    asd

--- ver_mensajes.php
+++ ver_mensajes.php
@@ -7,10 +7,10 @@ $dbname = "form";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Verificar la conexión
 if ($conn->connect_error) {
     die("Error en la conexión a la base de datos: " . $conn->connect_error);
 }
+echo "Conexión exitosa"; // Este mensaje se mostrará si la conexión es exitosa.
 
 // Obtén los valores del formulario
 $email = $_POST["email"];
@@ -32,24 +32,3 @@ if (!empty($email) && !empty($name) && !empty($message)) {
 } else {
     echo "Error al enviar el mensaje. Por favor, completa todos los campos obligatorios.";
 }
-
-// Preparar la consulta SQL para recuperar los mensajes
-$sql = "SELECT * FROM tabla_datos";
-$result = $conn->query($sql);
-
-// Verificar si hay resultados
-if ($result->num_rows > 0) {
-    // Recorrer los resultados y mostrar los mensajes
-    while ($row = $result->fetch_assoc()) {
-        echo "Nombre: " . $row["nombre"] . "<br>";
-        echo "Correo electrónico: " . $row["correo"] . "<br>";
-        echo "Teléfono: " . $row["telefono"] . "<br>";
-        echo "Mensaje: " . $row["mensaje"] . "<br><br>";
-    }
-} else {
-    echo "No se encontraron mensajes.";
-}
-
-// Cerrar la conexión
-$conn->close();
-?>
